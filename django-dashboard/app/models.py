b0344bc65ea5d329317472277f42c77fcd4d0886
An Yi <anyiikb@gmail.com>
2021-04-29 08:55:18 +0700

    Add models for postgresql

--- django-dashboard/app/models.py
+++ django-dashboard/app/models.py
@@ -3,8 +3,36 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from typing import cast
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import OneToOneField
+from authentication import models as aut
 
 # Create your models here.
+
+# Save information of transfer money from 2 account
+class tranfer(models.Model):
+    sender = models.ForeignKey(aut.user, on_delete=CASCADE, related_name="sender")
+    receiver = models.ForeignKey(aut.user, on_delete=CASCADE, related_name="receiver")
+
+    amount = models.IntegerField()
+    note = models.TextField()
+    time = models.DateTimeField()
+
+# Save information of draw or deposit money 
+class change_balanced(models.Model):
+    account = models.ForeignKey(aut.user, on_delete=CASCADE, related_name="account")
+    staff = models.ForeignKey(aut.user, on_delete=CASCADE, related_name="staff_take_money") # customer will draw or deposit with staff, not online
+
+    amount = models.IntegerField()
+    note = models.TextField()
+    time = models.DateTimeField()
+
+
+
+
+
+
 
--- django-dashboard/authentication/models.py
+++ django-dashboard/authentication/models.py
@@ -1,8 +1,27 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class user(models.Model):
+    username = models.CharField(max_length=16, primary_key=True)
+    firstname = models.CharField(max_length=10)
+    lastname = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=10)
+
+class customer(models.Model):
+    username = models.ForeignKey(user, on_delete=models.CASCADE)
+    balanced = models.IntegerField(auto_created=0)
+
+class staff(models.Model):
+    username = models.ForeignKey(user, on_delete=models.CASCADE)
+
+class manager(models.Model):
+    username = models.ForeignKey(user, on_delete=models.CASCADE)
+
+class ITStaff(models.Model):
+    username = models.ForeignKey(user, on_delete=models.CASCADE)
+
+
+
+
